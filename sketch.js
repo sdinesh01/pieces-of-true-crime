8550a61cc67d55b5a0ad99ab241b6f6c9c6767a1
Shradha Dinesh <108999498+sdinesh01@users.noreply.github.com>
2024-05-06 12:54:30 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -52,8 +52,8 @@ function setup() {
     //create button to retrieve new text
     button1 = createButton("I need more information");
     button1.mouseClicked(restart);
-    button1.size(100, 70);
-    button1.position(10, 110);
+    button1.size(100, 90);
+    button1.position(10, 100);
     button1.style("background-color", "#93b9e1");
     button1.style("font-family", "monospace")
     button1.style("font-size", "14px");
